be7176381ba7e431750260d7f1781a767d56e1be
Kulkarni Manali <68278965+ManaliKulkarni30@users.noreply.github.com>
2021-05-11 12:17:18 +0530

    Update Header.py

--- Header.py
+++ Header.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
 
 
 #Logistic Regression
